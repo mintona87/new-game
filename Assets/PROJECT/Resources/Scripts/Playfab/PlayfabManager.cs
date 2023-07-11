f6f54426576ba43c1ab8558227f4b4a3addd1f06
realdecimalist <decimalist@protonmail.com>
2023-07-11 13:39:11 -0600

    commented out a line causing error in build

--- Assets/PROJECT/Resources/Scripts/Playfab/PlayfabManager.cs
+++ Assets/PROJECT/Resources/Scripts/Playfab/PlayfabManager.cs
@@ -9,7 +9,7 @@ using UnityEngine.UI;
 using System;
 using TMPro;
 using System.Threading.Tasks;
-using static UnityEditor.Progress;
+//using static UnityEditor.Progress;
 
 public class PlayfabManager : MonoBehaviour
 {
