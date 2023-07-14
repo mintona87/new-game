cfb66edccf6c411b192faea832b427bd0a0e0994
realdecimalist <decimalist@protonmail.com>
2023-07-14 14:37:44 -0600

    Added PlayerTitleText & FrameSlot placeholders to Player UI.

--- Assets/PROJECT/Resources/Scripts/Player/PlayerCombat.cs
+++ Assets/PROJECT/Resources/Scripts/Player/PlayerCombat.cs
@@ -489,11 +489,11 @@ public class PlayerCombat : MonoBehaviourPunCallbacks
                 targetScript.SetIsDefending(GetOtherPlayer() ,false);
                 targetScript.isDefendingTurnIndex = 0;
             }
-            playerManager.pv.RPC("ShowActionRPC", RpcTarget.AllBuffered, targetScript.playerUI.playerUsernameText.text + $" dealt {damage} damage!", localPlayerViewID, targetPlayerViewID, targetName);
+            playerManager.pv.RPC("ShowActionRPC", RpcTarget.AllBuffered, targetScript.playerUI.playerUsernameText.text + $" - {damage} ", localPlayerViewID, targetPlayerViewID, targetName);
         }
         else
         {
-            playerManager.pv.RPC("ShowActionRPC", RpcTarget.AllBuffered, playerManager.playerUI.playerUsernameText.text + " missed the attack!", localPlayerViewID, targetPlayerViewID, targetName);
+            playerManager.pv.RPC("ShowActionRPC", RpcTarget.AllBuffered, playerManager.playerUI.playerUsernameText.text + " MISS!", localPlayerViewID, targetPlayerViewID, targetName);
         }
 
         playerManager.TurnsSinceCharge++;
@@ -530,7 +530,7 @@ public class PlayerCombat : MonoBehaviourPunCallbacks
         int targetPlayerViewID = targetScript.pv.ViewID;
         string targetName = "localPlayer";
 
-        playerManager.pv.RPC("ShowActionRPC", RpcTarget.AllBuffered, playerManager.playerUI.playerUsernameText.text + $" healed for {healAmount}!", localPlayerViewID, targetPlayerViewID, targetName);
+        playerManager.pv.RPC("ShowActionRPC", RpcTarget.AllBuffered, playerManager.playerUI.playerUsernameText.text + $" + {healAmount}", localPlayerViewID, targetPlayerViewID, targetName);
 
     }
 
@@ -591,7 +591,7 @@ public class PlayerCombat : MonoBehaviourPunCallbacks
         int targetPlayerViewID = targetScript.pv.ViewID;
         string targetName = "target";
         
-        playerManager.pv.RPC("ShowActionRPC", RpcTarget.AllBuffered, targetScript.playerUI.playerUsernameText.text + $" dealt {damage} damage!", localPlayerViewID, targetPlayerViewID, targetName);
+        playerManager.pv.RPC("ShowActionRPC", RpcTarget.AllBuffered, targetScript.playerUI.playerUsernameText.text + $" - {damage} ", localPlayerViewID, targetPlayerViewID, targetName);
     }
     #endregion
 
